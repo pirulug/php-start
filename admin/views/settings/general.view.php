d5a6aa67df2c958fc08ec5389018914399bcb17d
Pirulug <pirulug@gmail.com>
2024-08-16 19:12:28 -0500

    Update general.view.php

--- admin/views/settings/general.view.php
+++ admin/views/settings/general.view.php
@@ -1,8 +1,8 @@
-<?php blockStart("style"); // Block?>
+<?php blockStart("style"); // Block ?>
 <link rel="stylesheet" href="<?= APP_URL ?>/admin/assets/css/tagify.css">
 <?php blockEnd("style"); ?>
 
-<?php blockStart("script"); // Block?>
+<?php blockStart("script"); // Block ?>
 <script src="<?= APP_URL ?>/admin/assets/js/tagify.js"></script>
 <script>
   const input = document.getElementById('tag-input');
@@ -21,33 +21,34 @@
       <h3 class="h5 m-0">General</h3>
       <hr>
       <div class="mb-3">
-        <lable class="form-label">Site Name</lable>
+        <label class="form-label">Site Name</label>
         <input class="form-control" type="text" value="<?= $settings->st_sitename ?>" name="st_sitename">
       </div>
       <div class="mb-3">
-        <lable class="form-label">Description</lable>
-        <textarea class="form-control" name="st_description" style="field-sizing: content;min-height: 3lh;"><?= $settings->st_description ?></textarea>
+        <label class="form-label">Description</label>
+        <textarea class="form-control" name="st_description"
+          style="field-sizing: content;min-height: 3lh;"><?= $settings->st_description ?></textarea>
       </div>
       <div class="mb-3">
-        <lable class="form-label">Keywords</lable>
+        <label class="form-label">Keywords</label>
         <input class="form-control" id="tag-input" type="text" value='<?= $settings->st_keywords ?>' name="st_keywords">
       </div>
       <h3 class="h5 m-0">Social</h3>
       <hr>
       <div class="mb-3">
-        <lable class="form-label">Facebook</lable>
+        <label class="form-label">Facebook</label>
         <input class="form-control" type="text" value="<?= $settings->st_facebook ?>" name="st_facebook">
       </div>
       <div class="mb-3">
-        <lable class="form-label">Twitter</lable>
+        <label class="form-label">Twitter</label>
         <input class="form-control" type="text" value="<?= $settings->st_twitter ?>" name="st_twitter">
       </div>
       <div class="mb-3">
-        <lable class="form-label">Instagram</lable>
+        <label class="form-label">Instagram</label>
         <input class="form-control" type="text" value="<?= $settings->st_instagram ?>" name="st_instagram">
       </div>
       <div class="mb-3">
-        <lable class="form-label">Youtube</lable>
+        <label class="form-label">Youtube</label>
         <input class="form-control" type="text" value="<?= $settings->st_youtube ?>" name="st_youtube">
       </div>
 
