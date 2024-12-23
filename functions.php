bcad9209d3c8013148b8c49964a154b53f31d61c
pirulug <pirulug@gmail.com>
2024-12-22 23:32:02 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,17 +11,3 @@ function get_user_session_information($connect) {
   $sentence = $sentence->fetch(PDO::FETCH_OBJ);
   return ($sentence) ? $sentence : false;
 }
-
-function setPageMetaData($page_title_suffix = "", $request = "") {
-  global $settings;
-  $page_title       = $page_title_suffix == "" ? $settings->st_sitename : $page_title_suffix . " | " . $settings->st_sitename;
-  $page_description = $settings->st_description;
-  $page_keywords    = $settings->st_keywords;
-
-  $og_title       = $page_title;
-  $og_description = $page_description;
-  $og_image       = SITE_URL . "/assets/img/logo-vertical.png";
-  $og_url         = SITE_URL . "/" . $request;
-
-  return compact('page_title', 'page_description', 'page_keywords', 'og_title', 'og_description', 'og_image', 'og_url');
-}
