31bff4f03abf86c20347349d997eabbc36684681
pirulug <pirulug@gmail.com>
2025-03-09 22:27:57 -0500

    Update info.php

--- admin/controllers/settings/info.php
+++ admin/controllers/settings/info.php
@@ -6,19 +6,19 @@ $accessControl->require_login(SITE_URL_ADMIN . "/controllers/login.php");
 $accessControl->check_access([1], SITE_URL . "/404.php");
 
 // Requisitos mínimos
-$minPhpVersion = '8.0.0';
-$requiredExtensions = ['curl', 'pdo_mysql', 'mbstring', 'gd','imagick'];
-$minMemoryLimit = 128; // en MB
-$maxExecutionTime = 30; // en segundos
-$minDbVersion = '8.0.0'; 
+$minPhpVersion      = '8.0.0';
+$requiredExtensions = ['curl', 'pdo_mysql', 'mbstring', 'gd', 'imagick'];
+$minMemoryLimit     = 128; // en MB
+$maxExecutionTime   = 30; // en segundos
+$minDbVersion       = '8.0.0';
 
 // Verificaciones
-$isPhpVersionOk = version_compare(phpversion(), $minPhpVersion, '>=');
+$isPhpVersionOk    = version_compare(phpversion(), $minPhpVersion, '>=');
 $enabledExtensions = get_loaded_extensions();
 $missingExtensions = array_diff($requiredExtensions, $enabledExtensions);
-$memoryLimit = (int) filter_var(ini_get('memory_limit'), FILTER_SANITIZE_NUMBER_INT);
-$isMemoryLimitOk = $memoryLimit >= $minMemoryLimit;
-$executionTime = (int) ini_get('max_execution_time');
+$memoryLimit       = (int) filter_var(ini_get('memory_limit'), FILTER_SANITIZE_NUMBER_INT);
+$isMemoryLimitOk   = $memoryLimit >= $minMemoryLimit;
+$executionTime     = (int) ini_get('max_execution_time');
 $isExecutionTimeOk = $executionTime >= $maxExecutionTime;
 
 // Iconos
@@ -28,13 +28,13 @@ function getStatusIcon($status) {
     : '<i class="fa-solid fa-x text-danger"></i>';
 }
 
-$phpVersion = phpversion();
+$phpVersion       = phpversion();
 $loadedExtensions = implode(', ', get_loaded_extensions());
-$serverSoftware = $_SERVER['SERVER_SOFTWARE'];
-$dbVersion = $connect->getAttribute(PDO::ATTR_SERVER_VERSION);
+$serverSoftware   = $_SERVER['SERVER_SOFTWARE'];
+$dbVersion        = $connect->getAttribute(PDO::ATTR_SERVER_VERSION);
 
 // Identificar el tipo de base de datos (MySQL o MariaDB)
-$query = $connect->query("SELECT @@version_comment AS dbType");
+$query  = $connect->query("SELECT @@version_comment AS dbType");
 $result = $query->fetch(PDO::FETCH_ASSOC);
 $dbType = $result['dbType'] ?? 'Desconocido';
 
@@ -42,6 +42,6 @@ $isDbVersionOk = version_compare($dbVersion, $minDbVersion, '>=');
 
 /* ========== Theme config ========= */
 $theme_title = "Informacion";
-$theme_path = "info";
+$theme_path  = "info";
 include BASE_DIR_ADMIN . "/views/settings/info.view.php";
 /* ================================= */
