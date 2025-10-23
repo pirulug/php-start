f0d485a9e3180482984b98080873256e5ec952c4
pirulug <pirulug@gmail.com>
2025-10-22 21:07:49 -0500

    Enhance user profile with role info and layout update
    Updated the profile controller to join user data with role information, allowing display of the user's role. Refactored the profile view to a two-column layout, showing detailed user and role information, and improved UI structure for better clarity.

--- app/admin/account/controllers/profile.php
+++ app/admin/account/controllers/profile.php
@@ -3,7 +3,17 @@
 // Obtener datos del usuario logeado
 $id_user = $_SESSION["user_id"];
 
-$query = "SELECT * FROM users WHERE user_id = :user_id";
+$query = "SELECT 
+  users.*,
+  roles.* 
+FROM 
+  users
+INNER JOIN
+  roles
+ON
+  users.role_id = roles.role_id
+WHERE 
+  user_id = :user_id";
 $stmt  = $connect->prepare($query);
 $stmt->bindParam(":user_id", $id_user);
 $stmt->execute();
--- app/admin/account/views/profile.view.php
+++ app/admin/account/views/profile.view.php
@@ -1,39 +1,76 @@
-<div class="card mb-3">
-  <div class="card-body p-4">
-    <div class="position-relative mb-5">
-      <img class="img-fluid w-100 rounded" src="https://dummyimage.com/1200x300/ddd/000.jpg" alt="Cover Image">
-      <div class="position-absolute top-100 start-50 translate-middle">
-        <img class="img-fluid rounded-circle border border-3 border-white shadow"
-          src="<?= SITE_URL ?>/uploads/user/<?= $user->user_image ?>" width="170" height="170"
-          alt="<?= $user->user_display_name ?>">
-      </div>
-    </div>
-    <div class="pt-5">
-      <h3 class="mb-1 text-center"><?= $user->user_display_name ?></h3>
-    </div>
-  </div>
-</div>
+<div class="row">
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="text-center">
+          <img class="img-fluid rounded mb-1" src="<?= SITE_URL ?>/uploads/user/<?= $user->user_image ?>"
+            alt="Profile Image">
+        </div>
 
-<div class="card">
-  <div class="card-header">
-    <h5 class="card-title mb-0">Activities</h5>
-  </div>
-  <div class="card-body h-100">
-    <div class="d-flex align-items-start mb-3">
-      <img class="rounded-circle me-2" src="<?= SITE_URL ?>/uploads/user/<?= $user->user_image ?>" width="36"
-        height="36" alt="<?= $user->user_display_name ?>">
-      <div class="flex-grow-1">
-        <small class="float-end text-primary">now</small>
-        <strong><?= $user->user_display_name ?></strong>
-        pushed new commits to <strong>GitHub</strong>
-        <br>
-        <small class="text-muted">Today</small>
+        <h3 class="text-center"><?= $user->user_display_name ?></h3>
+
+        <p class="text-muted text-center"><?= $user->role_name ?></p>
+
+        <ul class="list-group list-group-flush mb-1">
+          <li class="list-group-item">
+            <b>Username</b>
+            <a class="pull-right"><?= $user->user_name ?></a>
+          </li>
+          <li class="list-group-item">
+            <b>Last Login</b>
+            <a class="pull-right"><?= $user->user_last_login ?></a>
+          </li>
+          <li class="list-group-item">
+            <b>Member Since </b>
+            <a class="pull-right"><?= $user->user_created ?></a>
+          </li>
+        </ul>
+
+        <div class="d-grid">
+          <a href="http://localhost:8050/profile/index/edit" class="btn btn-primary btn-block">
+            <b>
+              <i class="fa fa-pencil"></i>
+              Edit
+            </b>
+          </a>
+        </div>
       </div>
     </div>
-    <hr>
-    <div class="d-grid">
-      <a class="btn btn-primary" href="https://github.com/Pirulug" target="_blank">See
-        more on GitHub</a>
+  </div>
+  <div class="col-md-9">
+    <div class="card">
+      <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered table-striped">
+          <tbody>
+            <tr>
+              <td width="160"><strong>Name</strong>:</td>
+              <td><?= $user->user_display_name ?></td>
+            </tr>
+            <tr>
+              <td><strong>Username</strong>:</td>
+              <td><?= $user->user_name ?></td>
+            </tr>
+            <tr>
+              <td><strong>Email</strong>:</td>
+              <td><?= $user->user_email ?></td>
+            </tr>
+            <tr>
+              <td><strong>Role</strong>:</td>
+              <td><?= $user->role_name ?></td>
+            </tr>
+            <tr>
+              <td><strong>Last Login</strong>:</td>
+              <td><?= $user->user_last_login ?></td>
+            </tr>
+            <tr>
+              <td><strong>Member Since</strong>:</td>
+              <td><?= $user->user_created ?></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </div>
     </div>
   </div>
 </div>
